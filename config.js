5d5d410214c60dfc240cc93d72ec681fad4e1499
ffpwlgbarkhausen-admin <ffpw.lgbarkhausen@gmail.com>
2026-03-12 09:57:07 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    BASE_URL: "https://script.google.com/macros/s/AKfycbwX2b-JeXWIXEpT4gzgvnUx53LY-Xn7e9i4oAeTYL4HLE20tNpWOd9_KisM52Qqc8B1/exec"
+    BASE_URL: "https://script.google.com/macros/s/AKfycbxxoC8gRFmN1E-RmRGZxoSzbBX8oo5VtLR01_SXo0Sr7ilGJXsUaWdyncNhQsAV7Tcq/exec"
 };
